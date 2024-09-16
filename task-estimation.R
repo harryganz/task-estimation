4ede6c6697adcdd228c525ec1f77c82d29d3e290
Harry Ganz <hganz@leagueapps.com>
2024-09-16 14:01:14 -0400

    Updates figure

--- task-estimation.R
+++ task-estimation.R
@@ -18,13 +18,14 @@ tasks <- read_csv('tasks.csv') |>
 tasks |>
   ggplot(aes(x = `Time Estimate (hours)`, y = `Time Logged (hours)`)) +
   geom_point() +
-  geom_smooth(method = "lm", se = FALSE, aes(color = 'Logged', linetype = 'Logged')) +
+  geom_smooth(method = "lm", se = FALSE, aes(color = 'Actual', linetype = 'Actual')) +
   geom_segment(aes(x = 0.25, xend = 2, y = 0.25, yend = 2, col = 'Ideal', linetype = 'Ideal'), lwd = 1) +
   scale_discrete_manual(
     c("color", "linetype"),
     name = NULL,
-    breaks = c("Logged", "Ideal"),
-    values = c("Logged" = 4, "Ideal"= 1)
+    breaks = c("Actual", "Ideal"),
+    values = c("Actual" = 4, "Ideal"= 1)
   ) +
   theme_minimal() +
   theme(legend.position = "bottom")
+ggsave("./task-estimation.png", height = 5.63, width = 4.16, units = "in", dpi = 150)
